620b4173d54568aeec030ebf5aab6edb0ba5d053
jdgwartney <davidg@boundary.com>
2014-10-15 13:12:50 -0700

    New files

---
+++ src/main/webapp/config/dashboard-config.js
@@ -0,0 +1,37 @@
+{
+	"pages": {
+		[
+		 {
+			 "id": "home",
+			 "title": "Cisco TV - Monitoring Dashboards",
+			 "panels": [
+			            {
+			            	"id": "panel-1",
+			            	"dtitle": "Jabber Servers",
+			            	"src": "https://premium.boundary.com/embed/e.67e9eb2f6a?d-w=3&d-h=2&d-pad=0&d-header=0&d-slider=0&d-legend=1&d-bg=3D3D3D&d-g-MYSQL_BYTES_IN=0-0-1-1-t&d-g-MYSQL_BYTES_OUT=0-0-1-1-b&d-sg-cpu=0-1-1-1&d-sg-hdrb=1-1-1-1-t&d-sg-hdwb=1-1-1-1-b&d-sg-hdr=2-1-1-1-t&d-sg-hdw=2-1-1-1-b&d-sg-ni=1-0-1-1-t&d-sg-no=1-0-1-1-b&d-g-MYSQL_CONNECTIONS=2-0-1-1-t&d-g-MYSQL_COMMITS=2-0-1-1-b#null"
+			            },
+			            {
+			            	"id": "panel-2",
+			            	"dtitle": "Java Servers",
+			            	"src": ""
+			            },
+			            {
+			            	"id":"panel-3",
+			            	"dtitle": "MySQL Servers",
+			            	"src"
+			            }
+			            ]
+		 },
+		 {
+			 "id": "jabba-servers",
+			 "title": "Cisco TV - Monitoring Dashboards",
+			 "panels": [
+			            {
+			            	"id": "panel-1",
+			            	"dtitle": "Java Servers"
+			            }
+			            ]
+		 }
+		 ]
+	}
+}
