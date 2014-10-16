51ceff0cc598d0b9ce3c4a46e3ad71d58a993b92
jdgwartney <davidg@boundary.com>
2014-10-15 20:40:39 -0700

    First cut at using jQuery to set panel data'

--- src/main/webapp/assets/js/dashboard.js
+++ src/main/webapp/assets/js/dashboard.js
@@ -2,14 +2,24 @@
  * 
  */
 function configureDashboard() {
+	
+	$("title").text("XXXXXXXXXXXXXX");
     
-    $("title").text("XXXXXXXX");
-    
-    $("#panel-1").find("dtitle").text("NEW TITLE")
+    //$("#panel-1").find("dtitle").text("NEW TITLE")
     
     $.getJSON("config/dashboard.json",function(data) {
-    	  page = data[$("html").attr("id")]
-    	  $("title").text(page.title);;
+  	  page = data[$("html").attr("id")];
+
+    	  if (page != null) {
+    		  
+    		  // Get the panels loop through
+    		  panels = page.panels;
+        	  for (i in panels) {
+        		  p = panels[i]
+        		  $(p.id).find("dtitle").text(p.dtitle);
+        		  $(p.id).find("iframe").attr("src",p.src);
+        	  }
+    	  }
     	});
 	
 }
