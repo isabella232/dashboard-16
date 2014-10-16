43c613475dd911ace685878c1eed1155a039a8fa
jdgwartney <davidg@boundary.com>
2014-10-15 21:13:46 -0700

    Working version of JavaScript that loads configuration in pages

--- src/main/webapp/assets/js/dashboard.js
+++ src/main/webapp/assets/js/dashboard.js
@@ -2,25 +2,25 @@
  * 
  */
 function configureDashboard() {
-	
-	$("title").text("XXXXXXXXXXXXXX");
-    
-    //$("#panel-1").find("dtitle").text("NEW TITLE")
-    
-    $.getJSON("config/dashboard.json",function(data) {
-  	  page = data[$("html").attr("id")];
 
-    	  if (page != null) {
-    		  
-    		  // Get the panels loop through
-    		  panels = page.panels;
-        	  for (i in panels) {
-        		  p = panels[i]
-        		  $(p.id).find("dtitle").text(p.dtitle);
-        		  $(p.id).find("iframe").attr("src",p.src);
-        	  }
-    	  }
-    	});
-	
+	$.getJSON("config/dashboard.json", function(data) {
+		// Use the id attribute of the html tag to lookup our page object
+		// associated with this page
+		page = data[$("html").attr("id")];
+
+		// We do not find the page then there is nothing to do
+		if (page != null) {
+
+			// Get the panels and loop through them
+			panels = page.panels;
+			for (i in panels) {
+				p = panels[i]
+				// The div id is used to identify each panel
+				// set the appropriate data for each of them
+				$(p.id).find("dtitle").text(p.dtitle);
+				$(p.id).find("iframe").attr("src",p.src);
+			}
+		}
+	});
+
 }
-
