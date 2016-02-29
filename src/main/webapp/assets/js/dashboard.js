252c2613407cc3de5e52cc3b9d6972a9e6d0669e
Gwartney <davidg@boundary.com>
2016-02-29 03:55:31 -0800

    Remove boundary references

--- src/main/webapp/assets/js/dashboard.js
+++ src/main/webapp/assets/js/dashboard.js
@@ -3,26 +3,26 @@
  */
 function configureDashboard() {
 
-	$.getJSON("dashboard.json", function(data) {
-		// Use the id attribute of the html tag to lookup our page object
-		// associated with this page
-		page = data[$("html").attr("id")];
-		
-		$("title").text(page.title)
+    $.getJSON("dashboard.json", function (data) {
+        // Use the id attribute of the html tag to lookup our page object
+        // associated with this page
+        var page = data[$("html").attr("id")];
 
-		// We do not find the page then there is nothing to do
-		if (page != null) {
+        $("title").text(page.title)
 
-			// Get the panels and loop through them
-			panels = page.panels;
-			for (i in panels) {
-				p = panels[i]
-				// The div id is used to identify each panel
-				// set the appropriate data for each of them
-				$(p.id).find("dtitle").text(p.dtitle);
-				$(p.id).find("iframe").attr("src",p.src);
-			}
-		}
-	});
+        // if we do not find the page then there is nothing to do
+        if (page != null) {
+
+            // Get the panels array and loop through them
+            var panels = page.panels;
+            for (var i in panels) {
+                p = panels[i]
+                // The div id is used to identify each panel
+                // set the appropriate data for each of them
+                $(p.id).find("dtitle").text(p.dtitle);
+                $(p.id).find("iframe").attr("src", p.src);
+            }
+        }
+    });
 
 }
